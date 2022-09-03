31c7fb925d1b17e06cf710a9c68d41189170e288
Kosta <kkoossttaa02@gmail.com>
2022-09-03 21:02:13 +0300

    second commit

--- script.js
+++ script.js
@@ -29,4 +29,17 @@ console.log(4 + +"5");
 let incr = 10,
     decr = 10;
 
+++incr;
+--decr;
 
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2+2*2 === '8');
+
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked || !isClose);
