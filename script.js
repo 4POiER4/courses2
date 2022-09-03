1c586bf00137685e5aaf5f2b2528412cac545f06
Kosta <kkoossttaa02@gmail.com>
2022-09-03 23:34:55 +0300

    hw1 !

--- script.js
+++ script.js
@@ -23,23 +23,43 @@
 
 // alert(`hello , ${user}`);
 
-console.log('arr' + " - object");
-console.log(4 + +"5");
+// console.log('arr' + " - object");
+// console.log(4 + +"5");
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-++incr;
---decr;
+// ++incr;
+// --decr;
 
-console.log(++incr);
-console.log(--decr);
+// console.log(++incr);
+// console.log(--decr);
 
-console.log(5%2);
+// console.log(5%2);
 
-console.log(2+2*2 === '8');
+// console.log(2+2*2 === '8');
 
-const isChecked = true,
-      isClose = true;
+// const isChecked = true,
+//       isClose = true;
 
-console.log(isChecked || !isClose);
+// console.log(isChecked || !isClose);
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false
+};
+
+const a = prompt('Один из последних фильмов', ''),
+      b = prompt('оценка', ''),
+      c = prompt('Один из последних фильмов', ''),
+      d = prompt('оценка', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+      
+console.log(personalMovieDB);
