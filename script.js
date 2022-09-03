fb72fbdd7d07b1fa73b1c175a99386e84249054d
Kosta <kkoossttaa02@gmail.com>
2022-09-03 21:00:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict"
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('What you name', '');
+// answers[1] = prompt('What you second name', '');
+// answers[2] = prompt('how old are you', '');
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`hello , ${user}`);
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+
