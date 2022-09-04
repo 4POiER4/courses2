c3e518dbd61e9f3b45c17206cf5d67bb6fa8225e
Kosta <kkoossttaa02@gmail.com>
2022-09-04 16:01:09 +0300

    Yslovie

--- script.js
+++ script.js
@@ -44,22 +44,55 @@
 
 // console.log(isChecked || !isClose);
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    geners: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     geners: [],
+//     privat: false
+// };
 
-const a = prompt('Один из последних фильмов', ''),
-      b = prompt('оценка', ''),
-      c = prompt('Один из последних фильмов', ''),
-      d = prompt('оценка', '');
+// const a = prompt('Один из последних фильмов', ''),
+//       b = prompt('оценка', ''),
+//       c = prompt('Один из последних фильмов', ''),
+//       d = prompt('оценка', '');
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
+//       personalMovieDB.movies[a] = b;
+//       personalMovieDB.movies[c] = d;
       
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// if (4==9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error');
+// }
+
+// if (num<49){
+//     console.log('Error');
+// }else if (num>100){
+//     console.log('many');
+// }else{
+//     console.log('Good');
+// }
+
+// (num === 50) ? console.log('OK') : console.log('Error');
+
+const num = 50;
+
+switch (num) {
+    case 49: 
+        console.log('False');
+        break;
+    case 100:
+        console.log('False');
+        break;
+    case 51: 
+        console.log('true');
+        break;
+    default: 
+        console.log('not good not bad');
+        break;
+}
